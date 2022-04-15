ac9a1dc5ddaa6e929543708beca191020c9ba6d5
Chakrapani Upadhyaya <98646256+SourceLastBenchCoder@users.noreply.github.com>
2022-04-15 18:59:46 +0530

    Initial Commit

---
+++ My.Simple.Web.API/My.Simple.Web.API/Controllers/LearningController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using My.Simple.Web.API.Models;
+
+namespace My.Simple.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LearningController : ControllerBase
+    {
+        private static AppDbContext _context;
+
+        public LearningController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("learning-list")]
+        public IActionResult Get()
+        {
+            return Ok(_context.Learning.ToList());
+        }
+
+        [HttpPost("learning-create")]
+        public IActionResult Create(Learning learning)
+        {
+            _context.Learning.Add(learning);
+            _context.SaveChanges();
+            return Ok("success");
+        }
+
+        [HttpPost("learning-update")]
+        public IActionResult Update(Learning learning, int learningId)
+        {
+            var learningToUpdate = _context.Learning.Where(o => o.LearningId == learningId).FirstOrDefault();
+
+            if (learningToUpdate == null)
+                return BadRequest();
+
+            learningToUpdate.Title = learning.Title;
+            learningToUpdate.Description = learning.Description;
+            learningToUpdate.DateStarted = learning.DateStarted;
+            learningToUpdate.DateCompleted = learning.DateCompleted;
+
+
+            _context.Learning.Update(learningToUpdate);
+            _context.SaveChanges();
+            return Ok("success");
+        }
+
+        [HttpDelete("learning-delete")]
+        public IActionResult Delete(int learningId)
+        {
+            var learningToDelete = _context.Learning.Where(o => o.LearningId == learningId).FirstOrDefault();
+
+            if (learningToDelete == null)
+                return BadRequest();
+
+            _context.Learning.Remove(learningToDelete);
+            _context.SaveChanges();
+            return Ok("success");
+        }
+    }
+}
---
+++ My.Simple.Web.API/My.Simple.Web.API/Models/Learning.cs
@@ -0,0 +1,11 @@
+﻿namespace My.Simple.Web.API.Models
+{
+    public class Learning
+    {
+        public int LearningId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateStarted { get; set; }
+        public DateTime DateCompleted { get; set; }
+    }
+}
