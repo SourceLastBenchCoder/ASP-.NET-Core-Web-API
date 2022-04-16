6f7325a958ce8c4ae892d7ef3b804efee1a52950
Chakrapani Upadhyaya <98646256+SourceLastBenchCoder@users.noreply.github.com>
2022-04-16 19:48:34 +0530

    ADDED CORS ENABLED PROJECT

---
+++ My.Simple.Web.API With CORS/My.Simple.Web.API/Migrations/20220415090112_Initial Database Create.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace My.Simple.Web.API.Migrations
+{
+    public partial class InitialDatabaseCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Learning",
+                columns: table => new
+                {
+                    LearningId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateStarted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCompleted = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Learning", x => x.LearningId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Learning");
+        }
+    }
+}
---
+++ My.Simple.Web.API With CORS/My.Simple.Web.API/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using My.Simple.Web.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager Configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "SampleCORS", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("SampleCORS");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
