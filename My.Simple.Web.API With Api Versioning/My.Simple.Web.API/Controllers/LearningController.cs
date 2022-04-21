3cb84325df8c7a9806a6b97944ac6992cb900703
Chakrapani Upadhyaya <98646256+SourceLastBenchCoder@users.noreply.github.com>
2022-04-21 09:46:05 +0530

    Api Versioning added

---
+++ My.Simple.Web.API With Api Versioning/My.Simple.Web.API/Controllers/LearningController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using My.Simple.Web.API.Models;
+
+namespace My.Simple.Web.API.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/Learning")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [EnableCors("SampleCORS")]
+    public class LearningController : ControllerBase
+    {
+        private static AppDbContext _context;
+
+        public LearningController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("learning-list")]
+        [MapToApiVersion("1.0")]
+        public IActionResult Get()
+        {
+            return Ok(_context.Learning.ToList());
+        }
+
+        [HttpGet("learning-list")]
+        [MapToApiVersion("2.0")]
+        public IActionResult Get2()
+        {
+            return Ok("Hello I am version 2");
+        }
+
+        [HttpPost("learning-create")]
+        public IActionResult Create(Learning learning)
+        {
+            _context.Learning.Add(learning);
+            _context.SaveChanges();
+            return Ok("success");
+        }
+
+        [HttpPost("learning-update")]
+        public IActionResult Update(Learning learning, int learningId)
+        {
+            var learningToUpdate = _context.Learning.Where(o => o.LearningId == learningId).FirstOrDefault();
+
+            if (learningToUpdate == null)
+                return BadRequest();
+
+            learningToUpdate.Title = learning.Title;
+            learningToUpdate.Description = learning.Description;
+            learningToUpdate.DateStarted = learning.DateStarted;
+            learningToUpdate.DateCompleted = learning.DateCompleted;
+
+
+            _context.Learning.Update(learningToUpdate);
+            _context.SaveChanges();
+            return Ok("success");
+        }
+
+        [HttpDelete("learning-delete")]
+        public IActionResult Delete(int learningId)
+        {
+            var learningToDelete = _context.Learning.Where(o => o.LearningId == learningId).FirstOrDefault();
+
+            if (learningToDelete == null)
+                return BadRequest();
+
+            _context.Learning.Remove(learningToDelete);
+            _context.SaveChanges();
+            return Ok("success");
+        }
+    }
+}
---
+++ My.Simple.Web.API With Api Versioning/My.Simple.Web.API/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using My.Simple.Web.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager Configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "SampleCORS", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+    });
+});
+
+builder.Services.AddApiVersioning(option =>
+{
+    option.DefaultApiVersion = new ApiVersion(1, 0);
+    option.AssumeDefaultVersionWhenUnspecified = true;
+    option.ReportApiVersions = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("SampleCORS");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
